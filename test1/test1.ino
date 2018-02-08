34ef4724748ee9d52437f1c034203e277dc20caf
DesislavaBalkandzhieva <dessibalk@gmail.com>
2018-02-08 15:41:49 +0200

    upload 1st version for test- works

---
+++ test1/test1.ino
@@ -0,0 +1,16 @@
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  float voltage, in;
+  in= analogRead(A0);
+  voltage = mapfloat(in, 0, 1023, 0, 10);
+  Serial.println(voltage);
+  delay(50);
+}
+
+float mapfloat(long x, long in_min, long in_max, long out_min, long out_max)
+{
+ return (float)(x - in_min) * (out_max - out_min) / (float)(in_max - in_min) + out_min;
+}
