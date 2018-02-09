c789318077935edd7aa5d188700bf6e5a7813dfe
Vencislav Nachev <vbn_94@abv.bg>
2018-02-09 11:35:22 +0200

    Update test1.ino
    idealno

--- test1/test1.ino
+++ test1/test1.ino
@@ -16,3 +16,5 @@ float mapfloat(long x, long in_min, long in_max, long out_min, long out_max)
 {
  return (float)(x - in_min) * (out_max - out_min) / (float)(in_max - in_min) + out_min;
 }
+
+// super e
