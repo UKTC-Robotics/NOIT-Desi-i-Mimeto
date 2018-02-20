38d795f0575a7dd6aeb603d902e61a5c65711909
DesislavaBalkandzhieva <36234147+DesislavaBalkandzhieva@users.noreply.github.com>
2018-02-20 15:19:47 +0200

    test 2

---
+++ desi_i_mimeto.ino
@@ -0,0 +1,18 @@
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  float voltage, in;
+  float range= 10; // goren obhvat na izmervane
+  in= analogRead(A0);
+  voltage = mapfloat (in, 0, 1023, 0, range);
+  Serial.println(voltage);
+  delay(50);
+}
+
+
+float mapfloat(long x, long in_min, long in_max, long out_min, long out_max)
+{
+ return (float)(x - in_min) * (out_max - out_min) / (float)(in_max - in_min) + out_min;
+}
