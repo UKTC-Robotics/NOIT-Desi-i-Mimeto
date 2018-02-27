63dc88905ee89be6c7d03c7ea9faa10f7d216438
DesislavaBalkandzhieva <36234147+DesislavaBalkandzhieva@users.noreply.github.com>
2018-02-27 18:24:22 +0200

    chast 2
    napravihme suprotivlenieto i veche raboti

---
+++ suprotivlenie.ino
@@ -0,0 +1,25 @@
+int R=10000;
+int Uin=5;
+float Rx = 0;
+
+
+
+void setup() {
+ Serial.begin(9600);
+}
+
+void loop() {
+  int a = analogRead(A3);
+  float Uout= mapfloat(a, 0, 1023, 0, 5);
+  if(Uin == Uout)
+    Rx = 0;
+  else
+    Rx= (Uout*R)/(Uin-Uout);
+  Serial.println(Rx);
+  delay(500);
+}
+
+float mapfloat(long x, long in_min, long in_max, long out_min, long out_max)
+{
+  return (float)(x - in_min) * (out_max - out_min) / (float)(in_max - in_min) + out_min;
+}
