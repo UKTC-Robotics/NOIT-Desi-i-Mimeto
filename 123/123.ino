83f08332c6e312148dc6f3ae5f6cc2e14502bf7a
DesislavaBalkandzhieva <36234147+DesislavaBalkandzhieva@users.noreply.github.com>
2018-03-06 08:26:44 -0800

    Arduino code for sending values to Python

---
+++ 123/123.ino
@@ -0,0 +1,27 @@
+
+
+int a, b, c, d, e;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  a = analogRead(A0);
+  b = analogRead (A1);
+  c = analogRead (A2);
+  d = analogRead (A3);
+  e = analogRead (A4);
+  Serial.print("#");
+  Serial.print(a);
+  Serial.print("#");
+  Serial.print(b);
+  Serial.print("#");
+  Serial.print(c);
+  Serial.print("#");
+  Serial.print(d);
+  Serial.print("#");
+  Serial.print(e);
+  Serial.println ("#");
+  delay(500);
+}
