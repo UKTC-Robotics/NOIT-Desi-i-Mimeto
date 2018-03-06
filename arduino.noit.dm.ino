15e981f82db57a95e6e7304275ccae36df32faec
StoyanovaMaria <36234224+StoyanovaMaria@users.noreply.github.com>
2018-03-06 16:51:27 +0200

    Arduino code wards Python

---
+++ arduino.noit.dm.ino
@@ -0,0 +1,22 @@
+
+
+
+char c;
+void setup() {
+  Serial.begin(9600);
+  pinMode(13, OUTPUT);
+}
+
+void loop() {
+  if(Serial.available() > 0){
+    c = Serial.read();
+  }
+
+  if(c == 'h')
+    digitalWrite(13, HIGH);
+
+  if(c == 'l')
+    digitalWrite(13, LOW);
+
+  delay(20);
+}
